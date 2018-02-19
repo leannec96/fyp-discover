8c171ec758a2833ab035529cfcd774abcdfb27b0
Leanne Cassidy <leannecassidy07@hotmail.com>
2018-02-19 15:54:49 +0000

    signup alone

--- inc/signup.php
+++ inc/signup.php
@@ -16,7 +16,40 @@
     
   
       ?>
-      
+      <!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/custom.css">
+<title>Welcome to Discover</title>
+<meta name="author" content="Jake Rocheleau">
+  <link rel="stylesheet" type="text/css" media="all" href="css/styles.css">
+  <link href="https://bootswatch.com/4/lux/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+</head>
+<body>
+
+ <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Discover</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" style="">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="landingpage.php">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="signup.php">Register/Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
       <div class="signup">
         <div class="panel panel-primary" align="middle">
           <div class="panel-heading" align="middle">
@@ -114,3 +147,5 @@
     </div>
     </br>
     </br>
+</body>
+</html>
--- signup.php
+++
@@ -1,8 +0,0 @@
-<?php  
-  include 'init.php';
-  include 'inc/htmlstart.php';
-  include 'inc/navbar.php';
-  include 'inc/signup2.php';
-  include 'inc/htmlend.php';
-
- ?>
