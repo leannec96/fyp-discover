e5b451983668c0c6010fd4c06cd47f169619aefa
Leanne Cassidy <leannecassidy07@hotmail.com>
2018-02-19 15:42:10 +0000

    index change 2

--- index.php
+++ index.php
@@ -1,4 +1,56 @@
-<?php  
-  include_once('landingpage.php');
+<?php
+include 'init.php';
+include 'inc/middleware.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/custom.css">
+<title>Welcome to Discover</title>
+<meta name="author" content="Jake Rocheleau">
+  <link rel="stylesheet" type="text/css" media="all" href="css/styles.css">
+  <link href="https://bootswatch.com/4/lux/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="libs/js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="libs/js/bootstrap.min.js"></script>
+</head>
+<body>
 
- ?>
+ <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Discover</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" style="">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="landingpage.php">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="inc/signup.php">Register/Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+        <div class="jumbotron" align="middle">
+  <h1 class="display-3">Welcome to Discover</h1>
+  </br>
+    <p class="lead">Thinking of persuing a course but you aren't sure if it's the right one for you?</p>
+  <hr class="my-4">
+<p>Chat to current college students to discover interests you didn't realise you had, courses you never heard of and longterm rewards! </p>
+  <p class="lead">
+      </br>
+ <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+       
+        </body>
+        </html>
