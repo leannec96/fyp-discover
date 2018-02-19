cf91492e84bcd25914b87cf6a5baf8205df98028
Leanne Cassidy <leannecassidy07@hotmail.com>
2018-02-19 15:47:04 +0000

    trying again with heroku db

--- inc/db.php
+++ inc/db.php
@@ -2,17 +2,17 @@
 
 
 // for c9
-	$server = 'localhost';
-	$username= 'root';
-	$password='';
-	$db = 'chatapp';
+	// $server = 'localhost';
+	// $username= 'root';
+	// $password='';
+	// $db = 'chatapp';
 	
 	
 	// //for heroku
-	// 	$server = 'd4f9cda4@eu-cdbr-west-02.cleardb.net';
-	// $username= 'b94bc67ee56667';
-	// $password='d4f9cda4';
-	// $db = 'heroku_09f39c0d0356122';
+		$server = 'd4f9cda4@eu-cdbr-west-02.cleardb.net';
+	$username= 'b94bc67ee56667';
+	$password='d4f9cda4';
+	$db = 'heroku_09f39c0d0356122';
 
 	$conn = mysqli_connect($server,$username,$password,$db);
 	$conn->set_charset('utf8');
